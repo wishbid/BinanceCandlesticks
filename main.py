223512486c3123f9fc697a3337b7921a61a00429
postonlylosses <markus.ventris@gmail.com>
2023-07-16 21:33:20 +1000

    init commit

---
+++ main.py
@@ -0,0 +1,75 @@
+import ccxt
+import json
+import arrow
+import argparse
+import dateparser
+
+
+def main():
+    timeframes_set = {'1m', '3m', '5m', '15m', '30m', '1h', '4h', '8h', '12h', '1d', '1w', '1M'}
+
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument('-s', '--start_time', required=True, type=str,
+                        help='use whatever format you want, ("YYYY.MM.DD", "YYYY.mm.dd:HH:MM:SS")')
+    parser.add_argument('-e', '--end_time', required=True)
+    parser.add_argument('-x', '--symbol', required=True,
+                        help='trading symbol, etc [BTC/USDT, ETH/USDT]')
+    parser.add_argument('-t', '--timeframe', default='1m', choices=timeframes_set,
+                        help='candlesticks timeframe')
+    parser.add_argument('-f', '--filename', default='historical_data',
+                        help='file name to save file')
+    parser.add_argument('-r', '--rate_limit', default=125, const=True, type=int, nargs='?',
+                        help='rate limit override, you probably dont need to touch this')
+    args = parser.parse_args()
+
+    exchange = ccxt.binance({
+        'rateLimit': 125,  # adjust to avoid hitting rate limits
+        'enableRateLimit': True,
+    })
+
+    symbol = args.symbol
+    timeframe = args.timeframe  # 1 minute
+
+    start = dateparser.parse(args.start_time)
+    end = dateparser.parse(args.end_time)
+    print(f'Start: \t{arrow.get(start)}')
+    print(f'End: \t{arrow.get(end)}')
+
+    start_timestamp = int(start.timestamp())
+    end_timestamp = int(end.timestamp())
+
+    since = start_timestamp * 1000
+    end = end_timestamp * 1000
+
+    data = []
+
+    # Loop over the time range and fetch OHLCV data
+
+    while since < end:
+        try:
+            ohlcv = exchange.fetch_ohlcv(symbol, timeframe, since, limit=1500)
+            print(f'Last time received {arrow.get(ohlcv[-1][0])}')
+            if len(ohlcv) == 0:
+                break
+            data.extend(ohlcv)
+            since = ohlcv[-1][0] + 1  # get start timestamp of next minute
+        except Exception as e:
+            print(f'Fetch failed: {e}')
+            break
+
+    # Convert to JSON and save to file
+    with open(args.filename + '.json', 'w') as f:
+        for line in data:
+
+            # Sometimes we grab more data than we need from the exchange. Break early if thats the case.
+            if line[0] >= end:
+                break
+
+            f.write(json.dumps(
+                {'timestamp': line[0], 'open': line[1], 'high': line[2], 'low': line[3], 'close': line[4],
+                 'volume': line[5]}) + "\n")
+
+
+if __name__ == "__main__":
+    main()
---
+++ readme.md
@@ -0,0 +1,35 @@
+# Crypto Historical Data Fetcher
+
+This script allows you to fetch historical cryptocurrency data from Binance. You can specify the symbol pair, the timeframe, and the start and end times to gather data. The data is saved as a JSON file.
+
+## Dependencies
+
+Please install these before use.
+- ccxt
+- json
+- arrow
+- argparse
+- dateparser
+
+## Usage
+
+```
+python <script_name>.py -s <start_time> -e <end_time> -x <symbol> -t <timeframe>
+```
+
+### Arguments
+
+- `-s`, `--start_time`: Start time of the range for which you want the historical data. You can use pretty much any format you want, e.g. ("YYYY.MM.DD", "YYYY.mm.dd HH:MM:SS")
+- `-e`, `--end_time`: End time of the range for which you want the historical data.
+- `-x`, `--symbol`: The trading symbol pair, e.g., "BTC/USDT", "ETH/USDT".
+- `-t`, `--timeframe`: The timeframe for the data. Options are '1m', '3m', '5m', '15m', '30m', '1h', '4h', '8h', '12h', '1d', '1w', '1M'.
+- `-f`, `--filename`: Filename for the output file. Default is 'historical_data'.
+- `-r`, `--rate_limit`: Rate limit override. Default is 125 (No need to change its pretty fast).
+
+### Example
+
+```
+python <script_name>.py -s "2023.01.01" -e "2023.07.16" -x "BTC/USDT" -t "1h"
+```
+
+This will fetch the historical OHLCV data for the BTC/USDT pair from 1st January 2023 to 16th July 2023 in 1-hour timeframes and save it to a file named 'btc_usdt_data.json'.
